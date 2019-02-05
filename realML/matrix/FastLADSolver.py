983de7c7143b736f2e4712c0fea23250005b8e0f
Alex Gittens <gittea@rpi.edu>
2019-02-05 16:25:11 -0500

    again, was pointing to wrong source urls in primitive jsons

--- realML/matrix/FastLADSolver.py
+++ realML/matrix/FastLADSolver.py
@@ -67,17 +67,17 @@ class FastLAD(SupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, Hyperparam
             'name': __author__,
             'contact': 'mailto:gittea@rpi.edu',
             'uris' : [
-                'https://github.com/ICSI-RealML/realML.git',
+                'https://github.com/alexgittens/realML.git',
             ],
         },
         'installation': [
             {
                 'type': 'PIP',
-                'package_uri': 'git+https://github.com/ICSI-RealML/realML.git@{git_commit}#egg=realML'.format(git_commit=utils.current_git_commit(os.path.dirname(__file__)))
+                'package_uri': 'git+https://github.com/alexgittens/realML.git@{git_commit}#egg=realML'.format(git_commit=utils.current_git_commit(os.path.dirname(__file__)))
             }
         ],
         'location_uris': [ # NEED TO REF SPECIFIC COMMIT
-            'https://github.com/ICSI-RealML/realML/blob/master/realML/matrix/FastLADSolver.py',
+            'https://github.com/alexgittens/realML/blob/master/realML/matrix/FastLADSolver.py',
             ],
         'preconditions': [
             'NO_MISSING_VALUES',
