eb86d7f532b85abbdb905c7bb89061648b8dbe6d
Alex <gittea@rpi.edu>
2019-06-11 18:59:17 +0000

    changed meta info for the Gaussian KRR pipeline to try and eliminate key error when loading training data

--- realML/pipelines/base.py
+++ realML/pipelines/base.py
@@ -24,6 +24,15 @@ class BasePipeline(object):
     def get_id(self):
         return self._pipeline.id
 
+    def genmeta(self, dataset):
+        meta_info = {
+            'problem': dataset + "_problem",
+            'full_inputs': [ dataset + "_dataset" ],
+            'train_inputs': [ dataset + "_dataset_TRAIN" ],
+            'test_inputs': [ dataset + "_dataset_TEST" ],
+            'score_inputs': [ dataset + "_dataset_SCORE" ]}
+        return meta_info
+
     def get_json(self):
         # Make it pretty.
         return json.dumps(json.loads(self._pipeline.to_json()), indent = 4)
