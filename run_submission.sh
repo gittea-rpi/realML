fca74f499653838e40c30558facc31d0ed090852
Ben Erichson <erichson@berkeley.edu>
2019-06-12 10:54:15 -0700

    update

--- run_submission.sh
+++ run_submission.sh
@@ -1,3 +1,8 @@
 pip3 install -e .
-python3 genjsons.py
-rm -rf ../primitives/v
+python3 genjson.py
+rm -rf ../primitives/v2019.6.7/ICSI/
+cp -r ICSI ../primitives/v2019.6.7/
+git --git-dir=../primitives/.git --work-tree=../primitives add -u
+git --git-dir=../primitives/.git --work-tree=../primitives add v2019.6.7/ICSI/*
+git --git-dir=../primitives/.git --work-tree=../primitives commit -m "updating ICSI primitives and pipelines"
+git --git-dir=../primitives/.git --work-tree=../primitives push
