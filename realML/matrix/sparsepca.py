9d40120cd132798da3452f656116d0fd5107bd55
erichson <erichson@uw.edu>
2019-06-14 15:17:08 -0700

    debug

--- realML/matrix/sparsepca.py
+++ realML/matrix/sparsepca.py
@@ -128,10 +128,8 @@ class SparsePCA(UnsupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, Hyperp
         #enc.fit(self._training_inputs)
         #self._training_inputs = enc.transform(self._training_inputs).toarray()
         
-        self._training_inputs = np.array(self._training_inputs)
-        self._training_inputs[np.isnan(self._training_inputs)] = 1
-        
-        
+        #self._training_inputs = np.array(self._training_inputs)
+        #self._training_inputs[np.isnan(self._training_inputs)] = 1
         
         # Center data
         self._mean = self._training_inputs.mean(axis=0)
