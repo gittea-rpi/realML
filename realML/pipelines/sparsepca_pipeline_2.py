05bdd6b42ac77fd839e811b6ce9096a2bf4b01ca
erichson <erichson@uw.edu>
2019-06-14 15:14:22 -0700

    debug

--- realML/pipelines/sparsepca_pipeline_2.py
+++ realML/pipelines/sparsepca_pipeline_2.py
@@ -20,7 +20,7 @@ import d3m.primitives.classification.gradient_boosting
 #
 import d3m.primitives.regression.gradient_boosting
 
-class sparsepcaPipeline(BasePipeline):
+class sparsepcaPipeline2(BasePipeline):
 
     #specify one seed dataset on which this pipeline can operate
 
