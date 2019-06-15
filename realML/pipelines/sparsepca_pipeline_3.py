cebd337638d00843e4a4f4fd0c52de9c02f2b368
erichson <erichson@uw.edu>
2019-06-15 14:38:51 -0700

    tune

--- realML/pipelines/sparsepca_pipeline_3.py
+++ realML/pipelines/sparsepca_pipeline_3.py
@@ -130,12 +130,12 @@ class sparsepcaPipeline3(BasePipeline):
         step_7.add_hyperparameter(
             name = 'learning_rate',
             argument_type = ArgumentType.VALUE,
-            data = 0.01
+            data = 0.1
         )
         step_7.add_hyperparameter(
             name = 'max_depth',
             argument_type = ArgumentType.VALUE,
-            data = 5
+            data = 3
         )             
         step_7.add_output('produce')
         pipeline.add_step(step_7)
