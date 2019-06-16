b6fad66aefbce1084e8e9e5de3f775f33b084cd4
erichson <erichson@uw.edu>
2019-06-16 12:18:50 -0700

    gbm

--- realML/pipelines/sparsepca_pipeline_2.py
+++ realML/pipelines/sparsepca_pipeline_2.py
@@ -130,12 +130,12 @@ class sparsepcaPipeline2(BasePipeline):
         step_7.add_hyperparameter(
             name = 'n_estimators',
             argument_type = ArgumentType.VALUE,
-            data = 50000
+            data = 5000
         )
         step_7.add_hyperparameter(
             name = 'learning_rate',
             argument_type = ArgumentType.VALUE,
-            data = 0.002
+            data = 0.05
         )
         step_7.add_hyperparameter(
             name = 'max_depth',
