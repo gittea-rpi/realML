ecbdf16cc8f962a8828da7fb5e74489ba8a72752
erichson <erichson@uw.edu>
2019-06-16 11:58:23 -0700

    test

--- realML/pipelines/randomizedpolypca_pipeline_2.py
+++ realML/pipelines/randomizedpolypca_pipeline_2.py
@@ -98,7 +98,7 @@ class randomizedpolypcaPipeline2(BasePipeline):
         step_6.add_hyperparameter(
                name = 'n_components',
                argument_type = ArgumentType.VALUE,
-               data = 6
+               data = 4
         )
         step_6.add_hyperparameter(
                name = 'degree',
@@ -133,12 +133,12 @@ class randomizedpolypcaPipeline2(BasePipeline):
         step_8.add_hyperparameter(
             name = 'n_estimators',
             argument_type = ArgumentType.VALUE,
-            data = 10000
+            data = 100000
         )
         step_8.add_hyperparameter(
             name = 'learning_rate',
             argument_type = ArgumentType.VALUE,
-            data = 0.009
+            data = 0.005
         )
         step_8.add_hyperparameter(
             name = 'max_depth',
