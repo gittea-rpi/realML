972f15b99f3a605c3f72e1852291d47a189fa92d
erichson <erichson@uw.edu>
2019-06-16 13:18:50 -0700

    gbm

--- realML/pipelines/sparsepca_pipeline.py
+++ realML/pipelines/sparsepca_pipeline.py
@@ -84,7 +84,7 @@ class sparsepcaPipeline(BasePipeline):
         step_5.add_hyperparameter(
                name = 'n_components',
                argument_type = ArgumentType.VALUE,
-               data = 5
+               data = 10
         )
         step_5.add_hyperparameter(
                name = 'beta',
