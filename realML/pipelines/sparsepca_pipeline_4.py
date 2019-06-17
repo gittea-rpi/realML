27df0242db43c8b0a51e89a98f4489c69b3ec5c2
erichson <erichson@uw.edu>
2019-06-17 15:21:02 -0700

    new pipe

--- realML/pipelines/sparsepca_pipeline_4.py
+++ realML/pipelines/sparsepca_pipeline_4.py
@@ -216,17 +216,17 @@ class sparsepcaPipeline4(BasePipeline):
         step_11.add_hyperparameter(
             name = 'n_estimators',
             argument_type = d3m_base.ArgumentType.VALUE,
-            data = 1000
+            data = 5000
         )
         step_11.add_hyperparameter(
             name = 'learning_rate',
             argument_type = d3m_base.ArgumentType.VALUE,
-            data = 0.01
+            data = 0.005
         )
         step_11.add_hyperparameter(
             name = 'max_depth',
             argument_type = d3m_base.ArgumentType.VALUE,
-            data = 5
+            data = 4
         )                 
         step_11.add_output('produce')
         pipeline.add_step(step_11)        
