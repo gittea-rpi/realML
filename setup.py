42e89938a76a62ff51ce9d7d40e9f90f9e2ed96f
Ben Erichson <erichson@berkeley.edu>
2019-06-17 22:55:35 +0000

    version update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as file:
     readme = file.read()
 
 setup(name="realML",
-      version="2.8.0",
+      version="2.8.1",
       description="ICSI provided machine learning primitives for DARPA D3M project, focusing on fast kernel methods and matrix factorizations ",
       packages=find_packages(),
       entry_points={
