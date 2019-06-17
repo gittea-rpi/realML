88799ee23d1e267d97657db88f3ef93e0d04c981
erichson <erichson@uw.edu>
2019-06-17 15:04:36 -0700

    new pipe

--- realML/pipelines/__init__.py
+++ realML/pipelines/__init__.py
@@ -17,6 +17,8 @@ from .FastLAD_pipeline import FastLADPipeline
 from .sparsepca_pipeline import sparsepcaPipeline
 from .sparsepca_pipeline_2 import sparsepcaPipeline2
 from .sparsepca_pipeline_3 import sparsepcaPipeline3
+from .sparsepca_pipeline_4 import sparsepcaPipeline4
+
 from .robustsparsepca_pipeline import robustsparsepcaPipeline
 from .randomizedpolypca_pipeline import randomizedpolypcaPipeline
 from .randomizedpolypca_pipeline_2 import randomizedpolypcaPipeline2
@@ -37,6 +39,7 @@ __all__ = ["RFMPreconditionedGaussianKRRPipeline",
            "sparsepcaPipeline",
            "sparsepcaPipeline2",
            "sparsepcaPipeline3",
+           "sparsepcaPipeline4",           
            "robustsparsepcaPipeline",
            "randomizedpolypcaPipeline",
            "randomizedpolypcaPipeline2"]
