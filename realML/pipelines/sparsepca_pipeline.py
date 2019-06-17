6a34b4738bc2f1d20bf79b13bedc169f2429ab82
erichson <erichson@uw.edu>
2019-06-17 14:19:28 -0700

    tune

--- realML/pipelines/sparsepca_pipeline.py
+++ realML/pipelines/sparsepca_pipeline.py
@@ -159,12 +159,12 @@ class sparsepcaPipeline(BasePipeline):
         step_8.add_hyperparameter(
                name = 'beta',
                argument_type = d3m_base.ArgumentType.VALUE,
-               data = 1e-8
+               data = 1e-5
         ) 
         step_8.add_hyperparameter(
                name = 'alpha',
                argument_type = d3m_base.ArgumentType.VALUE,
-               data = 1e-4
+               data = 1e-2
         )         
         step_8.add_hyperparameter(
                name = 'degree',
