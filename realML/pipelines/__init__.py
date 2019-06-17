b66d5705e15512783bf01b46ce59b57aef48cdce
Alex <gittea@rpi.edu>
2019-06-17 01:01:00 +0000

    added pipelines for 26_radon_seed for all alex's regression primitives

--- realML/pipelines/__init__.py
+++ realML/pipelines/__init__.py
@@ -1,10 +1,17 @@
 from .RFMPreconditionedGaussianKRR_pipeline import RFMPreconditionedGaussianKRRPipeline
 from .RFMPreconditionedGaussianKRR_pipeline_196_autoMpg import RFMPreconditionedGaussianKRRPipeline_196_autoMpg
+from .RFMPreconditionedGaussianKRR_pipeline_26_radon_seed import RFMPreconditionedGaussianKRRPipeline_26_radon_seed
+
 from .RFMPreconditionedPolynomialKRR_pipeline import RFMPreconditionedPolynomialKRRPipeline
 from .RFMPreconditionedPolynomialKRR_pipeline_196_autoMpg import RFMPreconditionedPolynomialKRRPipeline_196_autoMpg
+from .RFMPreconditionedPolynomialKRR_pipeline_26_radon_seed import RFMPreconditionedPolynomialKRRPipeline_26_radon_seed
+
 from .TensorMachinesRegularizedLeastSquares_pipeline import TensorMachinesRegularizedLeastSquaresPipeline
 from .TensorMachinesRegularizedLeastSquares_pipeline_196_autoMpg import TensorMachinesRegularizedLeastSquaresPipeline_196_autoMpg
+from .TensorMachinesRegularizedLeastSquares_pipeline_26_radon_seed import TensorMachinesRegularizedLeastSquaresPipeline_26_radon_seed
+
 from .TensorMachinesBinaryClassification_pipeline import TensorMachinesBinaryClassificationPipeline
+
 from .L1LowRank_pipeline import L1LowRankPipeline
 from .FastLAD_pipeline import FastLADPipeline
 from .sparsepca_pipeline import sparsepcaPipeline
@@ -17,10 +24,13 @@ from .randomizedpolypca_pipeline_2 import randomizedpolypcaPipeline2
 
 __all__ = ["RFMPreconditionedGaussianKRRPipeline", 
            "RFMPreconditionedGaussianKRRPipeline_196_autoMpg", 
+           "RFMPreconditionedGaussianKRRPipeline_26_radon_seed", 
            "RFMPreconditionedPolynomialKRRPipeline",
            "RFMPreconditionedPolynomialKRRPipeline_196_autoMpg",
+           "RFMPreconditionedPolynomialKRRPipeline_26_radon_seed",
            "TensorMachinesRegularizedLeastSquaresPipeline",
            "TensorMachinesRegularizedLeastSquaresPipeline_196_autoMpg",
+           "TensorMachinesRegularizedLeastSquaresPipeline_26_radon_seed",
            "TensorMachinesBinaryClassificationPipeline",
            "L1LowRankPipeline",
            "FastLADPipeline",
