7f6e333593be4f79a0cd5d5d744a94a688a7d1ee
Ben Erichson <erichson@berkeley.edu>
2020-01-16 10:58:58 -0800

    update

--- realML/pipelines/randomizedpolypca_pipeline_2.py
+++ realML/pipelines/randomizedpolypca_pipeline_2.py
@@ -72,7 +72,7 @@ class randomizedpolypcaPipeline2(BasePipeline):
         # Step 3: imputer
         step_3 = meta_pipeline.PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
         step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
-        #step_3.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='replace')
+        step_3.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE, data='replace')
         #step_3.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE, data=True)
         step_3.add_output('produce')        
         pipeline.add_step(step_3)    
