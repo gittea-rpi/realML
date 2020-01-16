5aba8b0adf5de1bf839915f8ac155cfda2984a78
Ben Erichson <erichson@berkeley.edu>
2020-01-16 10:44:05 -0800

    fix

--- realML/pipelines/randomizedpolypca_pipeline_2.py
+++ realML/pipelines/randomizedpolypca_pipeline_2.py
@@ -19,7 +19,6 @@ import d3m.primitives.classification.gradient_boosting
 #
 import d3m.primitives.regression.gradient_boosting
 
-from d3m import index
 import d3m.primitives.data_cleaning.imputer
 
 from d3m import index
@@ -72,7 +71,7 @@ class randomizedpolypcaPipeline2(BasePipeline):
 
         # Step 2: imputer
         step_3 = meta_pipeline.PrimitiveStep(primitive=index.get_primitive('d3m.primitives.data_cleaning.imputer.SKlearn'))
-        step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.2.produce')
+        step_3.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
         step_3.add_output('produce')
         step_3.add_hyperparameter(name='return_result', argument_type=ArgumentType.VALUE,data='replace')
         step_3.add_hyperparameter(name='use_semantic_types', argument_type=ArgumentType.VALUE,data=True)
