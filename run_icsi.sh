6851b74a80d2d9d88aebe1adff462ee9f31fe238
erichson <erichson@uw.edu>
2020-01-18 13:15:57 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -1,20 +1,19 @@
-# chmod +x run_submission.sh
-@echo off 
+#!/bin/bash 
 
 
-set id_sparsePCA_pip3 = 8758bb1a-c4b4-42aa-8bff-8a06fff54980 
 
-
+id_sparsePCA_pip3="8758bb1a-c4b4-42aa-8bff-8a06fff54980" 
 
 ####################################################
 # SparsePCA  -- LL0_207_autoPrice_MIN_METADATA
 ####################################################
 
-python3 -m d3m runtime fit-score -p ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/%id_sparsePCA_pip3%.json -r ../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json -i ../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json -t ../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json -a ../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json -O ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/%id_sparsePCA_pip3%_run.yaml
+python3 -m d3m runtime fit-score -p ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/$id_sparsePCA_pip3.json -r ../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json -i ../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json -t ../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json -a ../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json -O ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/$id_sparsePCA_pip3_run.yaml
 
 mkdir ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipeline_runs
 
-gzip ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/%id_sparsePCA_pip3%_run.yaml
+gzip ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/$id_sparsePCA_pip3_run.yaml
 
-mv ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/%id_sparsePCA_pip3%_run.yaml.gz ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipeline_runs
+mv ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/$id_sparsePCA_pip3_run.yaml.gz ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipeline_runs
 
+echo "Run SparsePCA  for LL0_207_autoPrice_MIN_METADATA"
