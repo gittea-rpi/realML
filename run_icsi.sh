60f669c4629f4a0f0567af7e7d235ccd9fafe3c2
erichson <erichson@uw.edu>
2020-01-18 13:50:39 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -2,20 +2,21 @@
 
 
 
-id_sparsePCA_pip3="8758bb1a-c4b4-42aa-8bff-8a06fff54980" 
+id_sparsePCA_pip3="c971dd9f-2232-43a0-8104-088d109d5f94" 
 
 
 ####################################################
 # SparsePCA  -- LL0_207_autoPrice_MIN_METADATA
 ####################################################
 
-path_1="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/$id_sparsePCA_pip3.json"
+path_1="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.6/pipelines/$id_sparsePCA_pip3.json"
 path_2="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json"
 path_3="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json"
 path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
-path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json -O ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/$id_sparsePCA_pip3_run.yaml"
+path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_6="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.6/pipelines/$id_sparsePCA_pip3_run.yaml"
 
-run='python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5'
+run='python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6' 
 `echo $run`
 
 
