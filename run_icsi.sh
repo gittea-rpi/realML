1956a63f161be90daa73319912716fe036f7fc83
erichson <erichson@uw.edu>
2020-01-18 13:37:35 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -15,5 +15,8 @@ path_3="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207
 path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
 path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json -O ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.5/pipelines/$id_sparsePCA_pip3_run.yaml"
 
-python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5
-echo
+#echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5
+`echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5`
+
+
+
