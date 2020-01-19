01dc5691d18502f85d387fd60a6e5d2468001ca4
erichson <erichson@uw.edu>
2020-01-18 17:57:35 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -1,41 +1,23 @@
 #!/bin/bash 
 
+TensorMachinesBinaryClassificationPipeline_path="ICSI/d3m.primitives.classification.tensor_machines_binary_classification.TensorMachinesBinaryClassification/3.0.1"
+TensorMachinesBinaryClassificationPipeline_id="3f6d2464-7be0-4bf4-a5ab-8f510c951e1a"
 sparsepcaPipeline_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline_id="d10b5db8-edbc-4097-bcda-1daacd1d2cf6"
+sparsepcaPipeline_id="02c44e21-5f5d-4ea8-b815-25ec2fdcdafd"
 sparsepcaPipeline2_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline2_id="f3cf9da3-72f6-4651-855c-220fbf411513"
+sparsepcaPipeline2_id="a7f321d7-761a-4541-a3f7-84ce24af24f6"
 sparsepcaPipeline3_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline3_id="826a8f9f-e23b-419d-8569-c57fdee2f873"
+sparsepcaPipeline3_id="352f740d-4a6e-4758-9bb5-66dc916ba0a8"
 sparsepcaPipeline4_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline4_id="afe66dcb-3912-4fda-b886-63eb93041259"
+sparsepcaPipeline4_id="346fb1f7-6ddd-4d61-be92-e3932c983b35"
 robustsparsepcaPipeline_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.RobustSparsePCA/3.0.1"
-robustsparsepcaPipeline_id="5795cc7a-907d-4f71-a23c-1f4b13d1d9e5"
+robustsparsepcaPipeline_id="c52d2724-86cf-486b-a885-0cfda23f785d"
 randomizedpolypcaPipeline_path="ICSI/d3m.primitives.feature_extraction.pca_features.RandomizedPolyPCA/3.0.1"
-randomizedpolypcaPipeline_id="fddb38df-02dc-4511-8c19-2188b2e648a6"
-
+randomizedpolypcaPipeline_id="5a755c78-17ed-4379-bf95-f71a281dc1e6"
 randomizedpolypcaPipeline2_path="ICSI/d3m.primitives.feature_extraction.pca_features.RandomizedPolyPCA/3.0.1"
-randomizedpolypcaPipeline2_id="eec0da96-b9e9-463d-a5d0-e004ebdb319e"
+randomizedpolypcaPipeline2_id="c934876b-feb7-4215-8e78-7d3f60722e3a"
 
-####################################################
-echo 'RandomizedPolyPCA  -- uu5_heartstatlog - pipeline 2'
-####################################################
-id_run="${randomizedpolypcaPipeline2_id}_run"
-path_1="$randomizedpolypcaPipeline2_path/pipelines/$randomizedpolypcaPipeline2_id.json"
-path_2="../datasets/seed_datasets_current/SEMI_1053_jm1_MIN_METADATA/SEMI_1053_jm1_MIN_METADATA_problem/problemDoc.json"
-path_3="../datasets/seed_datasets_current/SEMI_1053_jm1_MIN_METADATA/SEMI_1053_jm1_MIN_METADATA_dataset/datasetDoc.json"
-path_4="../datasets/seed_datasets_current/SEMI_1053_jm1_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
-path_5="../datasets/seed_datasets_current/SEMI_1053_jm1_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
-path_6="$randomizedpolypcaPipeline2_path/pipelines/$id_run.yaml"
 
-echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
-
-`echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6`
-
-`echo mkdir $randomizedpolypcaPipeline2_path/pipeline_runs`
-
-`echo gzip $randomizedpolypcaPipeline2_path/pipelines/$id_run.yaml`
-
-`echo mv $randomizedpolypcaPipeline2_path/pipelines/$id_run.yaml.gz $randomizedpolypcaPipeline2_path/pipeline_runs`
 
 
 
@@ -44,10 +26,10 @@ echo 'SparsePCA  -- 26_radon_seed - pipeline 1'
 ####################################################
 id_run="${sparsepcaPipeline_id}_run"
 path_1="$sparsepcaPipeline_path/pipelines/$sparsepcaPipeline_id.json"
-path_2="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json"
-path_3="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json"
-path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
-path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_2="../datasets/seed_datasets_current/26_radon_seed_MIN_METADATA/26_radon_seed_MIN_METADATA_problem/problemDoc.json"
+path_3="../datasets/seed_datasets_current/26_radon_seed_MIN_METADATA/26_radon_seed_MIN_METADATA_dataset/datasetDoc.json"
+path_4="../datasets/seed_datasets_current/26_radon_seed_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
+path_5="../datasets/seed_datasets_current/26_radon_seed_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
 path_6="$sparsepcaPipeline_path/pipelines/$id_run.yaml"
 
 #echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
@@ -62,14 +44,14 @@ path_6="$sparsepcaPipeline_path/pipelines/$id_run.yaml"
 
 
 ####################################################
-echo 'SparsePCA  -- 534_cps_85_wages - pipeline 2'
+echo 'SparsePCA  -- 534_cps_85_wages_MIN_METADATA - pipeline 2'
 ####################################################
 id_run="${sparsepcaPipeline2_id}_run"
 path_1="$sparsepcaPipeline2_path/pipelines/$sparsepcaPipeline2_id.json"
-path_2="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json"
-path_3="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json"
-path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
-path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_2="../datasets/seed_datasets_current/534_cps_85_wages_MIN_METADATA/534_cps_85_wages_MIN_METADATA_problem/problemDoc.json"
+path_3="../datasets/seed_datasets_current/534_cps_85_wages_MIN_METADATA/534_cps_85_wages_MIN_METADATA_dataset/datasetDoc.json"
+path_4="../datasets/seed_datasets_current/534_cps_85_wages_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
+path_5="../datasets/seed_datasets_current/534_cps_85_wages_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
 path_6="$sparsepcaPipeline2_path/pipelines/$id_run.yaml"
 
 #echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
@@ -110,10 +92,10 @@ echo 'SparsePCA  -- 196_autoMpg - pipeline 4'
 ####################################################
 id_run="${sparsepcaPipeline4_id}_run"
 path_1="$sparsepcaPipeline4_path/pipelines/$sparsepcaPipeline4_id.json"
-path_2="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json"
-path_3="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json"
-path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
-path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_2="../datasets/seed_datasets_current/196_autoMpg_MIN_METADATA/196_autoMpg_MIN_METADATA_problem/problemDoc.json"
+path_3="../datasets/seed_datasets_current/196_autoMpg_MIN_METADATA/196_autoMpg_MIN_METADATA_dataset/datasetDoc.json"
+path_4="../datasets/seed_datasets_current/196_autoMpg_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
+path_5="../datasets/seed_datasets_current/196_autoMpg_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
 path_6="$sparsepcaPipeline4_path/pipelines/$id_run.yaml"
 
 #echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
