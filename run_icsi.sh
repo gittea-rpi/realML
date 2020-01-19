ca3025292f03c4a3807d685f64efbb4e6d89923c
Ben Erichson <erichson@berkeley.edu>
2020-01-18 20:28:08 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -4,22 +4,44 @@
 
 
 
+RFMPreconditionedGaussianKRRPipeline_path="ICSI/d3m.primitives.regression.rfm_precondition_ed_gaussian_krr.RFMPreconditionedGaussianKRR/3.0.1"
+RFMPreconditionedGaussianKRRPipeline_id="131b859b-d2c1-4a57-aa22-ba5810cae9f6"
 sparsepcaPipeline2_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline2_id="5f799c13-1925-460c-9b6a-2e01f9cebad7"
+sparsepcaPipeline2_id="939751fa-1c21-418d-8bbc-56136320df91"
 sparsepcaPipeline3_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline3_id="cbe261d2-59fe-473c-9ea5-903996e29ded"
+sparsepcaPipeline3_id="7ab63b6f-ebc8-4087-8ad8-7ac6137437ee"
 sparsepcaPipeline4_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline4_id="7d618a00-c0aa-4932-8546-f400367b47d5"
+sparsepcaPipeline4_id="e63b8c25-3abb-4618-82ce-060bb10091dd"
 robustsparsepcaPipeline_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.RobustSparsePCA/3.0.1"
-robustsparsepcaPipeline_id="d884ca05-b275-4825-ba6d-21943bb1df8f"
+robustsparsepcaPipeline_id="90e80f91-3ce9-49ef-8a59-dfe9661190ff"
 robustsparsepcaPipeline2_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.RobustSparsePCA/3.0.1"
-robustsparsepcaPipeline2_id="b3474a7e-4c1e-4831-86d0-861474ea21fe"
+robustsparsepcaPipeline2_id="1bb4bf84-6238-4acc-9df1-ffa91da6ec31"
 randomizedpolypcaPipeline_path="ICSI/d3m.primitives.feature_extraction.pca_features.RandomizedPolyPCA/3.0.1"
-randomizedpolypcaPipeline_id="f712890b-7564-4704-a60f-186aaee5b380"
+randomizedpolypcaPipeline_id="23ec9a22-c3b7-49a2-9082-8b69b788d741"
 randomizedpolypcaPipeline3_path="ICSI/d3m.primitives.feature_extraction.pca_features.RandomizedPolyPCA/3.0.1"
-randomizedpolypcaPipeline3_id="49118898-1331-41a3-946f-7f892ceb51da"
+randomizedpolypcaPipeline3_id="4f300672-cac5-49ca-890f-b83fec5571d3"
 
 
+####################################################
+echo 'RFMPreconditionedGaussianKRRP  -- LL0_207_autoPrice_MIN_METADATA'
+####################################################
+id_run="${RFMPreconditionedGaussianKRRPipeline_id}_run"
+path_1="$RFMPreconditionedGaussianKRRPipeline_path/pipelines/$RFMPreconditionedGaussianKRRPipeline_id.json"
+path_2="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json"
+path_3="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json"
+path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
+path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_6="$RFMPreconditionedGaussianKRRPipeline_path/pipelines/$id_run.yaml"
+
+#echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
+
+`echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6`
+
+`echo mkdir $robustsparsepcaPipeline_path/pipeline_runs`
+
+`echo gzip $RFMPreconditionedGaussianKRRPipeline_path/pipelines/$id_run.yaml`
+
+`echo mv $RFMPreconditionedGaussianKRRPipeline_path/pipelines/$id_run.yaml.gz $RFMPreconditionedGaussianKRRPipeline_path/pipeline_runs`
 
 
 
