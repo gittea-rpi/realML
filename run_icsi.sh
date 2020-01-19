6d87683406d47c90db09ceda3710f90e18a18ee0
erichson <erichson@uw.edu>
2020-01-19 12:41:03 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -55,14 +55,14 @@ path_6="$sparsepcaPipeline5_path/pipelines/$id_run.yaml"
 `echo mv $sparsepcaPipeline5_path/pipelines/$id_run.yaml.gz $sparsepcaPipeline5_path/pipeline_runs`
 
 ####################################################
-echo 'SparsePCA  -- LL1_terra_canopy_height_long_form_s4_100_MIN_METADATA - pipeline 6'
+echo 'SparsePCA  -- 59_LP_karate_MIN_METADATA - pipeline 6'
 ####################################################
 id_run="${sparsepcaPipeline6_id}_run"
 path_1="$sparsepcaPipeline6_path/pipelines/$sparsepcaPipeline6_id.json"
-path_2="../datasets/seed_datasets_current/LL1_terra_canopy_height_long_form_s4_100_MIN_METADATA/LL1_terra_canopy_height_long_form_s4_100_MIN_METADATA_problem/problemDoc.json"
-path_3="../datasets/seed_datasets_current/LL1_terra_canopy_height_long_form_s4_100_MIN_METADATA/LL1_terra_canopy_height_long_form_s4_100_MIN_METADATA_dataset/datasetDoc.json"
-path_4="../datasets/seed_datasets_current/LL1_terra_canopy_height_long_form_s4_100_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
-path_5="../datasets/seed_datasets_current/LL1_terra_canopy_height_long_form_s4_100_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_2="../datasets/seed_datasets_current/59_LP_karate_MIN_METADATA/59_LP_karate_MIN_METADATA_problem/problemDoc.json"
+path_3="../datasets/seed_datasets_current/59_LP_karate_MIN_METADATA/59_LP_karate_MIN_METADATA_dataset/datasetDoc.json"
+path_4="../datasets/seed_datasets_current/59_LP_karate_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
+path_5="../datasets/seed_datasets_current/59_LP_karate_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
 path_6="$sparsepcaPipeline6_path/pipelines/$id_run.yaml"
 
 #echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
