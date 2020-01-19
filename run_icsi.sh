4cad0e65dfa641ebcf0b916f8f5955f89db207e4
erichson <erichson@uw.edu>
2020-01-18 16:21:15 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -1,16 +1,35 @@
 #!/bin/bash 
 
+sparsepcaPipeline2_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
+sparsepcaPipeline2_id="d8a4a70d-4c52-434e-8bed-655540b35567"
+sparsepcaPipeline3_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
+sparsepcaPipeline3_id="43e4376b-90d6-4ce7-9672-3bbb628ccae8"
+robustsparsepcaPipeline_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.RobustSparsePCA/3.0.1"
+robustsparsepcaPipeline_id="9ce96e2d-112b-4a32-ae91-9bbaa52ecd00"
+randomizedpolypcaPipeline_path="ICSI/d3m.primitives.feature_extraction.pca_features.RandomizedPolyPCA/3.0.1"
+randomizedpolypcaPipeline_id="d152279d-6523-417f-b998-7dadde3787e8"
 
 
-sparsepcaPipeline3_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/2.8.6"
-sparsepcaPipeline3_id="4ca4c4da-8cba-44ef-b133-c97f804fbaae"
+####################################################
+echo 'SparsePCA  -- LL0_207_autoPrice_MIN_METADATA - pipeline 3'
+####################################################
+id_run="${sparsepcaPipeline2_id}_run"
+path_1="$sparsepcaPipeline2_path/pipelines/$sparsepcaPipeline2_id.json"
+path_2="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json"
+path_3="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json"
+path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
+path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_6="$sparsepcaPipeline2_path/pipelines/$id_run.yaml"
 
-robustsparsepcaPipeline_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.RobustSparsePCA/2.8.6"
-robustsparsepcaPipeline_id="0e946aa6-ea5b-4221-a184-8176e7efd37e"
+#echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
 
-randomizedpolypcaPipeline_path="ICSI/d3m.primitives.feature_extraction.pca_features.RandomizedPolyPCA/2.8.6"
-randomizedpolypcaPipeline_id="0eafa359-47a4-4d04-8ccd-d7a148e99312"
+`echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6`
 
+`echo mkdir $sparsepcaPipeline2_path/pipeline_runs`
+
+`echo gzip $sparsepcaPipeline2_path/pipelines/$id_run.yaml`
+
+`echo mv $sparsepcaPipeline2_path/pipelines/$id_run.yaml.gz $sparsepcaPipeline2_path/pipeline_runs`
 
 
 ####################################################
