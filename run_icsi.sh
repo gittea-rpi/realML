61d4e68104a184383d2a1d480940b54b19966b91
erichson <erichson@uw.edu>
2020-01-19 11:50:19 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -1,33 +1,49 @@
 #!/bin/bash 
 
-
 RFMPreconditionedGaussianKRRPipeline_path="ICSI/d3m.primitives.regression.rfm_precondition_ed_gaussian_krr.RFMPreconditionedGaussianKRR/3.0.1"
-RFMPreconditionedGaussianKRRPipeline_id="3c2e0f55-dd32-4c6c-80a5-a36e3af8f9e0"
-RFMPreconditionedGaussianKRRPipeline_26_radon_seed_path="ICSI/d3m.primitives.regression.rfm_precondition_ed_gaussian_krr.RFMPreconditionedGaussianKRR/3.0.1"
-RFMPreconditionedGaussianKRRPipeline_26_radon_seed_id="69a89cf6-ba2c-41db-aab9-64208e74680f"
+RFMPreconditionedGaussianKRRPipeline_id="edf3666d-39f5-401e-87a0-6662d317b68d"
+RFMPreconditionedGaussianKRRPipeline_196_autoMpg_path="ICSI/d3m.primitives.regression.rfm_precondition_ed_gaussian_krr.RFMPreconditionedGaussianKRR/3.0.1"
+RFMPreconditionedGaussianKRRPipeline_196_autoMpg_id="ee8df832-2320-4825-b90e-6e94b0c7f6a1"
 RFMPreconditionedPolynomialKRRPipeline_path="ICSI/d3m.primitives.regression.rfm_precondition_ed_polynomial_krr.RFMPreconditionedPolynomialKRR/3.0.1"
-RFMPreconditionedPolynomialKRRPipeline_id="c99abbf6-710e-4027-9fa1-6f7e328e81c7"
+RFMPreconditionedPolynomialKRRPipeline_id="d4b27376-c286-42a9-94d3-af9196f5752f"
+TensorMachinesRegularizedLeastSquaresPipeline_path="ICSI/d3m.primitives.regression.tensor_machines_regularized_least_squares.TensorMachinesRegularizedLeastSquares/3.0.1"
+TensorMachinesRegularizedLeastSquaresPipeline_id="2a9bb3fd-b6c7-4085-95b5-f75061062941"
 sparsepcaPipeline2_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline2_id="09ec5ff1-a907-4704-a00d-cbe77b3bbe06"
+sparsepcaPipeline2_id="2850c0e9-8cf7-4509-9ec4-f9e1626d5b69"
 sparsepcaPipeline3_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline3_id="4f5599c3-2ebb-4ca2-b673-34d481f50d7b"
+sparsepcaPipeline3_id="350456e0-25d1-41d9-9871-585dae873d82"
 sparsepcaPipeline4_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.SparsePCA/3.0.1"
-sparsepcaPipeline4_id="46a65cb6-be60-473e-a853-7e7b23dc6c7e"
+sparsepcaPipeline4_id="8a299366-726f-4d0d-ad84-a8903b0f9c47"
 robustsparsepcaPipeline_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.RobustSparsePCA/3.0.1"
-robustsparsepcaPipeline_id="1ff4b6ab-f1d4-482c-b5bd-4f04e897083d"
+robustsparsepcaPipeline_id="e1a4aec2-622e-45b6-89f7-d77fbe0f1dfc"
 robustsparsepcaPipeline2_path="ICSI/d3m.primitives.feature_extraction.sparse_pca.RobustSparsePCA/3.0.1"
-robustsparsepcaPipeline2_id="8cee6111-6d01-4f2d-a888-ea11d80c7ded"
+robustsparsepcaPipeline2_id="40ed69cc-6a3a-469e-8808-cdfbe47e310f"
 randomizedpolypcaPipeline_path="ICSI/d3m.primitives.feature_extraction.pca_features.RandomizedPolyPCA/3.0.1"
-randomizedpolypcaPipeline_id="451ea77d-af2b-4b63-98be-bdf2cee78416"
+randomizedpolypcaPipeline_id="28b90c0b-77b8-4fa8-8320-6e2257fbcc92"
 randomizedpolypcaPipeline3_path="ICSI/d3m.primitives.feature_extraction.pca_features.RandomizedPolyPCA/3.0.1"
-randomizedpolypcaPipeline3_id="9ffff3b3-e66b-47ca-be45-a984bd13fe35"
+randomizedpolypcaPipeline3_id="097d1fb3-110b-4fed-b12f-3822925bcdf5"
 
 
+####################################################
+echo 'TensorMachinesRegularizedLeastSquares  -- LL0_207_autoPrice_MIN_METADATA'
+####################################################
+id_run="${TensorMachinesRegularizedLeastSquaresPipeline_id}_run"
+path_1="$TensorMachinesRegularizedLeastSquaresPipeline_path/pipelines/$TensorMachinesRegularizedLeastSquaresPipeline_id.json"
+path_2="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_problem/problemDoc.json"
+path_3="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/LL0_207_autoPrice_MIN_METADATA_dataset/datasetDoc.json"
+path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
+path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_6="$TensorMachinesRegularizedLeastSquaresPipeline_path/pipelines/$id_run.yaml"
 
+#echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
 
+`echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6`
 
+`echo mkdir $TensorMachinesRegularizedLeastSquaresPipeline_path/pipeline_runs`
 
+`echo gzip $TensorMachinesRegularizedLeastSquaresPipeline_path/pipelines/$id_run.yaml`
 
+`echo mv $TensorMachinesRegularizedLeastSquaresPipeline_path/pipelines/$id_run.yaml.gz $TensorMachinesRegularizedLeastSquaresPipeline_path/pipeline_runs`
 
 
 ####################################################
@@ -51,6 +67,27 @@ path_6="$RFMPreconditionedGaussianKRRPipeline_path/pipelines/$id_run.yaml"
 
 `echo mv $RFMPreconditionedGaussianKRRPipeline_path/pipelines/$id_run.yaml.gz $RFMPreconditionedGaussianKRRPipeline_path/pipeline_runs`
 
+
+####################################################
+echo 'RFMPreconditionedGaussianKRRP  -- LL0_207_autoPrice_MIN_METADATA'
+####################################################
+id_run="${RFMPreconditionedGaussianKRRPipeline_196_autoMpg_id}_run"
+path_1="$RFMPreconditionedGaussianKRRPipeline_196_autoMpg_path/pipelines/$RFMPreconditionedGaussianKRRPipeline_196_autoMpg_id.json"
+path_2="../datasets/seed_datasets_current/196_autoMpg_MIN_METADATA/196_autoMpg_MIN_METADATA_problem/problemDoc.json"
+path_3="../datasets/seed_datasets_current/196_autoMpg_MIN_METADATA/196_autoMpg_MIN_METADATA_dataset/datasetDoc.json"
+path_4="../datasets/seed_datasets_current/196_autoMpg_MIN_METADATA/TEST/dataset_TEST/datasetDoc.json"
+path_5="../datasets/seed_datasets_current/196_autoMpg_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
+path_6="$RFMPreconditionedGaussianKRRPipeline_196_autoMpg_path/pipelines/$id_run.yaml"
+
+#echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
+
+`echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6`
+
+#`echo mkdir $RFMPreconditionedGaussianKRRPipeline_196_autoMpg_path/pipeline_runs`
+
+`echo gzip $RFMPreconditionedGaussianKRRPipeline_196_autoMpg_path/pipelines/$id_run.yaml`
+
+`echo mv $RFMPreconditionedGaussianKRRPipeline_196_autoMpg_path/pipelines/$id_run.yaml.gz $RFMPreconditionedGaussianKRRPipeline_196_autoMpg_path/pipeline_runs`
 
 
 
