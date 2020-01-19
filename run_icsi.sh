22db101727b8e1fb066be1c2029515ddfca583df
erichson <erichson@uw.edu>
2020-01-18 17:06:24 -0800

    update

--- run_icsi.sh
+++ run_icsi.sh
@@ -32,7 +32,7 @@ path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/da
 path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
 path_6="$sparsepcaPipeline_path/pipelines/$id_run.yaml"
 
-echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
+#echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
 
 `echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6`
 
@@ -54,7 +54,7 @@ path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/da
 path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
 path_6="$sparsepcaPipeline2_path/pipelines/$id_run.yaml"
 
-echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
+#echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
 
 `echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6`
 
@@ -98,7 +98,7 @@ path_4="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/TEST/da
 path_5="../datasets/seed_datasets_current/LL0_207_autoPrice_MIN_METADATA/SCORE/dataset_SCORE/datasetDoc.json"
 path_6="$sparsepcaPipeline4_path/pipelines/$id_run.yaml"
 
-echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
+#echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6
 
 `echo python3 -m d3m runtime fit-score -p $path_1 -r $path_2 -i $path_3 -t $path_4 -a $path_5 -O $path_6`
 
